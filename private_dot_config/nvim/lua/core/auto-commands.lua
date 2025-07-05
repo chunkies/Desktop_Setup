e42d10231d5d1abcd1fd6029b4969a80893d4127
tristan <sly.tristan1@gmail.com>
2025-07-06 02:21:18 +1000

    added auto commands for moving text with alt

--- private_dot_config/nvim/lua/core/auto-commands.lua
+++ private_dot_config/nvim/lua/core/auto-commands.lua
@@ -16,3 +16,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ timeout = 100 })
   end,
 })
+
+
+-- Move current line up and down
+vim.api.nvim_set_keymap('n', '<Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+
+-- Move selected lines up and down
+vim.api.nvim_set_keymap('v', '<Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move current line up and down
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Move selected lines up and down
+vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
