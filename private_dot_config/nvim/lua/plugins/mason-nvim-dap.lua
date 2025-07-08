2d652f3ba4ed26213336acf31e632865e031bc7b
tristan <sly.tristan1@gmail.com>
2025-07-08 22:01:45 +1000

    added dap

---
+++ private_dot_config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,115 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio", -- Required for nvim-dap-ui
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+
+      --js debugger
+
+
+
+      -- c# debug adapter
+      dap.adapters.coreclr = {
+        type = 'executable',
+        command = vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg",
+        args = { '--interpreter=vscode' }
+      }
+
+      dap.configurations.cs = {
+        {
+          type = "coreclr",
+          name = "Launch - NetCoreDbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+          end,
+        },
+      }
+
+      -- typescript and js debug adapter
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+            "${port}"
+          }
+        }
+      }
+
+
+      dap.configurations.typescript = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          sourceMaps = true,
+          protocol = "inspector",
+          console = "integratedTerminal",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach to process",
+          processId = require 'dap.utils'.pick_process,
+          cwd = "${workspaceFolder}",
+        }
+      }
+
+
+
+
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Key mappings for debugging
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/Continue Debugging" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step Out" })
+      vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "Set Conditional Breakpoint" })
+      vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "Open REPL" })
+      vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "Run Last Debugging Session" })
+      vim.keymap.set("n", "<Leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      ensure_installed = { "python", "delve", "codelldb", "netcoredbg" }, -- Add debuggers as needed
+      automatic_installation = true,
+    },
+  },
+}
