669f4fc2966de903d578b0ad790b2e21ce421aff
tristan <sly.tristan1@gmail.com>
2025-07-10 02:32:41 +1000

    modify keymaps

--- private_dot_config/nvim/lua/core/keymaps.lua
+++ private_dot_config/nvim/lua/core/keymaps.lua
@@ -1,8 +1,8 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Move between windows using Ctrl + Arrow Keys
 map("n", "<C-Left>", "<C-w>h", opts)
@@ -14,9 +14,7 @@ map("n", "<C-Up>", "<C-w>k", opts)
 map("n", "<C-a>", "<cmd>ggVG<cr>", opts)
 
 -- save auto format
-map("n", "<C-s>", "<cmd>w<cr>", opts)
-map("i", "<C-s>", "<cmd>w<cr>", opts)
-map("v", "<C-s>", "<cmd>w<cr>", opts)
+map({ "i", "v", "n" }, "<C-s>", "<cmd>w<cr>", opts)
 
 --force quit
 map("n", "<C-w>", "<cmd>wq<cr>", opts)
@@ -24,17 +22,18 @@ map("n", "<C-w>", "<cmd>wq<cr>", opts)
 -- file explorer
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
-
 -- Telescope
-map("n", "<C-p>", ":Telescope project<CR>", opts)
-map("n", "<space><space>", "<cmd>Telescope find_files<cr>", { desc = "Find files (Telescope)" })
+map("n", "<leader>fp", ":Telescope project<CR>", opts)
+map('n', '<leader>ff', "<cmd>Telescope find_files<cr>", { desc = 'Telescope find files' })
+map('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", { desc = 'Telescope live grep' })
+map('n', '<leader>fb', "<cmd>Telescope buffers<cr>", { desc = 'Telescope buffers' })
+map('n', '<leader>fh', "<cmd>Telescope help_tags<cr>", { desc = 'Telescope help tags' })
 
 -- Debugger
 map("n", "<leader>b", ":DapToggleBreakpoint<CR>", opts)
 map("n", "<leader>c", ":DapContinue<CR>", opts)
 map("n", "<leader>x", ":DapTerminate<CR>", opts)
 map("n", "<leader>o", ":DapStepOver<CR>", opts)
-
 
 -- Set up LSP keymaps
 map("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
@@ -50,20 +49,8 @@ map("n", "[d", vim.diagnostic.goto_prev, opts)
 map("n", "]d", vim.diagnostic.goto_next, opts)
 map("n", "K", vim.lsp.buf.hover, opts)
 
-
-
 -- Move current line up and down in Normal mode
-vim.api.nvim_set_keymap('n', '<A-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
-
--- Move selected lines up and down in Visual mode
-vim.api.nvim_set_keymap('v', '<A-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-
--- Move current line up and down
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
-
--- Move selected lines up and down
-vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+map({ 'n', 'v' }, '<A-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+map({ 'n', 'v' }, '<A-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+map({ 'n', 'v' }, '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+map({ 'n', 'v' }, '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
