efb6660653bcfe44d9d5ab97265c23c41a87c2c6
tristan <sly.tristan1@gmail.com>
2025-07-21 15:51:15 +1000

    i3 setup

---
+++ private_dot_config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Kill existing polybar instances
+killall -q polybar
+
+# Wait until they’re actually dead
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar mybar &
--- scripts/pre-setup.sh
+++ scripts/pre-setup.sh
@@ -2,7 +2,6 @@
 set -e
 
 apt_packages=(
-  steam
   fzf
   ripgrep
   fd-find
@@ -21,20 +20,35 @@ apt_packages=(
   aspnetcore-runtime-9.0
   golang-go
   git-flow
-  i3
-  picom
+  snapd
 )
 
-read -e -p "Do you want to install Non Dev tools aswell? y/n" installDevTools
-
+read -e -p "Do you want to install Non Dev tools? y/n" installDevTools
 
 if [ "$installDevTools" == "y" ]; then
 
   echo ">>> Installing Spotify"
   curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
-  fi
 
+  apt_packages+=(
+    steam
+  )
+
+fi
+
+read -e -p "Do you want to Setup i3? y/n" setupi3
+
+if [ "$setupi3" == "y" ]; then
+  apt_packages+=(
+    picom
+    i3
+    polybar
+    alsa-utils
+    acpi
+  )
+
+fi
 
 echo ">>> Installing node"
 # Download and install nvm:
@@ -44,10 +58,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 # Download and install Node.js:
 nvm install 22
-node -v # Should print "v22.17.0".
+node -v     # Should print "v22.17.0".
 nvm current # Should print "v22.17.0".
-npm -v # Should print "10.9.2".
-
+npm -v      # Should print "10.9.2".
 
 echo ">>> Installing typescript"
 npm install -g typescript
@@ -60,7 +73,6 @@ wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-
 echo ">>> Installing NeoVim..."
 cd ~/Downloads
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
@@ -69,31 +81,31 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo rm nvim-linux-x86_64.tar.gz
 
 echo ">>> Installing required APT packages..."
-for package in "${apt_packages[@]}"; do 
-    sudo apt-get install $package
+for package in "${apt_packages[@]}"; do
+  sudo apt-get install $package
 done
 
 # --- Kitty config ---
 KITTY_CONF="$HOME/.config/kitty"
 if [ ! -d "$KITTY_CONF" ]; then
-    echo ">>> Creating Kitty config..."
-    mkdir -p "$KITTY_CONF"
-    cp /usr/share/doc/kitty/examples/kitty.conf "$KITTY_CONF/" || true
+  echo ">>> Creating Kitty config..."
+  mkdir -p "$KITTY_CONF"
+  cp /usr/share/doc/kitty/examples/kitty.conf "$KITTY_CONF/" || true
 fi
 
 # --- Install Oh My Zsh if missing ---
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo ">>> Installing Oh My Zsh..."
-    RUNZSH=no CHSH=no sh -c \
-      "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo ">>> Installing Oh My Zsh..."
+  RUNZSH=no CHSH=no sh -c \
+    "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
-    echo "✔ Oh My Zsh already installed"
+  echo "✔ Oh My Zsh already installed"
 fi
 
 # --- Set Zsh as default shell if not already ---
 if [ "$SHELL" != "$(which zsh)" ]; then
-    echo ">>> Setting Zsh as default shell..."
-    chsh -s "$(which zsh)"
+  echo ">>> Setting Zsh as default shell..."
+  chsh -s "$(which zsh)"
 fi
 
 # --- Install Nerd Font for icons (used in Trouble, NvimTree, etc.) ---
@@ -113,4 +125,3 @@ mkdir ~/Desktop/Repos
 
 # --- Final Message ---
 echo "✔ All done!"
-
