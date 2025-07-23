97119836d2ba696a31fc979d5d27f23ff4fe807f
tristan <sly.tristan1@gmail.com>
2025-07-24 02:51:31 +1000

    formating

--- private_dot_config/nvim/lua/core/auto-commands.lua
+++ private_dot_config/nvim/lua/core/auto-commands.lua
@@ -1,22 +1,22 @@
 -- show float for lsp on hover
 vim.api.nvim_create_autocmd("CursorHold", {
-  callback = function()
-    vim.diagnostic.open_float(nil, { focusable = false })
-  end,
+	callback = function()
+		vim.diagnostic.open_float(nil, { focusable = false })
+	end,
 })
 
 -- highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ timeout = 100 })
-  end,
+	callback = function()
+		vim.highlight.on_yank({ timeout = 100 })
+	end,
 })
 
 -- Stop all active LSP clients to avoid "Job still running"
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    for _, client in pairs(vim.lsp.get_active_clients()) do
-      client.stop()
-    end
-  end,
+	callback = function()
+		for _, client in pairs(vim.lsp.get_active_clients()) do
+			client.stop()
+		end
+	end,
 })
